c8cf355d3e9cf1de2ff9716c285ed6d5157c472d
Itay Sidis <is874k@intl.att.com>
2018-02-07 15:55:25 +0200

    onCardSelected on card

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,20 @@ import './App.css';
 
 class App extends React.Component {
   render() {
+    const onCardSelected = (data: any) => {
+      console.log(data);
+    };
     return (
       <div className="App">
         <Panel>
           <h2>Your rules-free TV</h2>
           <Group>
-            <Card>80+ channels</Card>
-            <Card>100+ channels</Card>
+            <Card onCardSelected={onCardSelected} data={{id: '1', title: '60+ channels', price: '35$'}} />
+            <Card onCardSelected={onCardSelected} data={{id: '2', title: '80+ channels', price: '50$'}} />
           </Group>
           <Group>
-            <Card>120+ channels</Card>
-            <Card>140+ channels</Card>
+            <Card onCardSelected={onCardSelected} data={{id: '3', title: '100+ channels', price: '60$'}} />
+            <Card onCardSelected={onCardSelected} data={{id: '4', title: '120+ channels', price: '70$'}} />
           </Group>
         </Panel>
       </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 export interface CardProps {
-
+    onCardSelected: (cardData: any) => void;
+    data: {id: string | number, title: string, price: string };
 }
 export interface CardState {
 
@@ -8,8 +9,18 @@ export interface CardState {
 export default class Card extends React.Component<CardProps, CardState> {
     constructor(props: CardProps) {
         super(props);
+        this.onCardSelected = this.onCardSelected.bind(this);
+    }
+    onCardSelected() {
+        return this.props.onCardSelected(this.props.data);
     }
     render() {
-        return this.props.children;
+        return (
+            <div>
+                <h3>{this.props.data.title}</h3>
+                <div>{this.props.data.price}</div>
+                <div><button onClick={this.onCardSelected}>Select plan</button></div>
+            </div>
+        );
     }
 }
